c6a1b33676c3261d6e3677ce569b295c9fc13f44
nel-thedev <nelelias@gmail.com>
2023-07-13 14:23:00 -0400

    Skills tooltips

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,7 +7,14 @@ const Skills = () => {
       <h3 className="text-4xl font-light mb-10">Skills</h3>
       <div className="flex gap-12 flex-wrap px-10 justify-center items-center">
         {skills.map((skill, index) => {
-          return <img src={skill} key={index} className="w-20" />;
+          return (
+            <div className="group relative flex items-center justify-center">
+              <img src={skill.img} key={index} className="w-20 relative" />
+              <span class="pointer-events-none absolute -top-7  w-max opacity-0 transition-opacity group-hover:opacity-100 bg-indigo-50 py-1 px-2 rounded-lg ">
+                {skill.name}
+              </span>
+            </div>
+          );
         })}
       </div>
     </section>
